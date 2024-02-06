bf2f6838f1b127abe236b07881263b96faad023b
Giovanni D <gsdambrosio@gmail.com>
2024-02-05 20:01:27 -0800

    Template File

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define double long double
+#define int long long
+#define GioSDA int32_t
+
+using namespace std;
+
+void setIO(string name) {
+    if (name.size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    cout.tie(0);
+} 
+
+GioSDA main() {
+    return 0;
+}
